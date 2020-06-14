9ca96516ff5dac555335842fada051b4bfe78c09
Alanw <57735457+Alanw-git@users.noreply.github.com>
2020-06-14 09:21:42 +0800

    改动

--- Desktop/used/大创学习文件/MOPSO/PSO1/func_fitness.m
+++
@@ -1,9 +0,0 @@
-function [ result ] = func_fitness( pop )
-%OBJFUNCTION 求适应度，最小值
-%   待优化目标函数
-% x:　种群或者个体
-% result : 种群适应度
-objValue =  func_objValue(pop);
-result  = 4001 - objValue ;
-end
-
--- Desktop/used/大创学习文件/MOPSO/PSO1/func_objValue.m
+++
@@ -1,8 +0,0 @@
-function [ result ] = func_objValue( pop )
-%FUNC_OBJVALUE 计算目标函数
-%   此处显示详细说明
-objValue =  sum(pop.^2,2);
-result = objValue ;
-
-end
-
